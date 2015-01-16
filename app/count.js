95949a8aedf8d7e18eae578181b7c0bf6730020b
prufrock123 <shawnmllr86@gmail.com>
2015-01-16 14:27:29 -0600

    working on count

--- app/count.js
+++ app/count.js
@@ -1,8 +1,45 @@
-if (typeof define !== 'function') { var define = require('amdefine')(module); }
+if (typeof define !== 'function') {
+    var define = require('amdefine')(module);
+}
 
-define(function () {
-  return {
-    count : function (start, end) {
-    }
-  };
+define(function() {
+    return {
+        count: function(start, end) {
+            /**
+             * My error below was that I was using setInterval which messed up 
+             * the tests.
+             */
+            // var i = start;
+            // debugger;
+
+            // function logger (iteration) {
+            // 	console.log(iteration);
+            // 	iteration++;
+            // }
+
+            // function stopper (intID) {
+            // 	clearInterval(intID);
+            // }
+
+            // var ticker = setInterval(function() {
+            // 	if (i <= end) {
+            // 		logger(i);
+            // 	} else {
+            // 		stopper(ticker);
+            // 	}
+            // }, 100);
+
+            // var ticker = setInterval(function() {
+            // 	// debugger;
+            //     if (i <= end) {
+            //         console.log(i);
+            //         i++;
+            //     } else {
+            //         clearInterval(ticker)
+            //     }
+            // }, 100);
+
+
+        }
+    };
 });
