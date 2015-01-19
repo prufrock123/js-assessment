560aa318d23e03cdfa2969eee6b21ad9f0505beb
prufrock123 <shawnmllr86@gmail.com>
2015-01-19 02:29:43 -0600

    finishes objects.

--- app/objects.js
+++ app/objects.js
@@ -3,15 +3,23 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function() {
   return {
     alterContext : function(fn, obj) {
-
+    	return fn.apply(obj)
     },
 
     alterObjects : function(constructor, greeting) {
-
+    	constructor.prototype.greeting = greeting;
     },
 
     iterate : function(obj) {
+    	var returnArray = [];
 
+    	for (var key in obj) {
+    		if (obj.hasOwnProperty(key)){
+				returnArray.push(key + ": " + obj[key])
+    		}
+    	}
+
+    	return returnArray;
     }
   };
 });
