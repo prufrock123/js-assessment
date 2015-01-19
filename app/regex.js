d19f96684f8c662143d7e9c060e7c145efe32416
prufrock123 <shawnmllr86@gmail.com>
2015-01-19 03:03:42 -0600

    Working on regex.

--- app/regex.js
+++ app/regex.js
@@ -3,15 +3,16 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function() {
   return {
     containsNumber : function(str) {
+        return /(\d)/g.test(str);
 
     },
 
     containsRepeatingLetter : function(str) {
-
+        return /([a-zA-Z]).*?\1/g.test(str)
     },
 
     endsWithVowel : function(str) {
-
+        return /(\d).
     },
 
     captureThreeNumbers : function(str) {
